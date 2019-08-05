7a4a97ddd5fd96dcc4386539ac701a65bcb6c762
Ian Shepherd <ianshepherd77@gmail.com>
2019-08-05 21:12:27 +0100

    Added add player to array

--- specs/team_specs.rb
+++ specs/team_specs.rb
@@ -4,33 +4,43 @@ require_relative('../teams')
 
 class TestTeams < MiniTest::Test
 
-  def test_team_name
-    team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
-    assert_equal('England', team_details.team_name)
-  end
+  def test_add_new_player()
+     team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
+     new_team = ['player 1', 'player 2', 'player 3', 'player 4']
+     assert_equal(new_team, team_details.add_new_player("player 4"))
+   end
+
+#    def test_add_new_player()
+#   team = Teams.new("Dolphins", ["Player1", "Player2", "Player3"], "Coachy McCoachFace")
+#   new_team = ["Player1", "Player2", "Player3", "Player4"]
+#   assert_equal(new_team, team.add_new_player("Player4"))
+# end
 
 
-  def test_players
-    team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
-    assert_equal(['player 1', 'player 2', 'player 3'], team_details.team_players)
-  end
+  # def test_team_name
+  #   team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
+  #   assert_equal('England', team_details.team_name)
+  # end
+  #
+  # def test_players
+  #   team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
+  #   assert_equal(['player 1', 'player 2', 'player 3'], team_details.team_players)
+  # end
+  #
+  # def test_coach_name
+  #   team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
+  #   assert_equal('mr coach', team_details.team_coach)
+  # end
 
-  def test_coach_name
-    team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
-    assert_equal('mr coach', team_details.team_coach)
-  end
+  # def test_set_coach_name
+  #   team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
+  #   team_details.set_coach_name('ferguson')
+  #   assert_equal('ferguson', team_details.set_coach_name())
+  # end
 
-  def test_set_coach_name
-    team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
-    team_details.new_coach = "ferguson"
-    assert_equal('ferguson', team_details.team_coach)
 
-  end
 
-  def test_add_player
-    team_details = Teams.new('England', ['player 1', 'player 2', 'player 3'], "mr coach")
-    team_details.team_players.push()
-  end
+
 
 
 end
--- teams.rb
+++ teams.rb
@@ -1,15 +1,19 @@
 class Teams
 
   attr_accessor :team_name, :players, :coach
+
   def initialize(input_team_name, input_array_of_players, input_coach)
     @team_name = input_team_name
     @players = input_array_of_players
     @coach = input_coach
+    @points = 0
   end
 
-  def adds_new_player_to_players_array(new_player_to_add)
-    team_details[:team_players].push(new_player_to_add)
+  def add_new_player(new_player)
+    @players << new_player
   end
+
+
 
   # def team_name()
   #   return @team_name
@@ -22,7 +26,13 @@ class Teams
   # def team_coach()
   #   return @coach
   # end
-  # def new_coach(new_coach_name)
+
+  # def set_coach_name(new_coach_name)
   #   @coach = new_coach_name
   # end
+
+
+
+
+
 end
